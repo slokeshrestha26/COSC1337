070697b1b6e26ac4ba4203b34df040850266859c
Sloke Shrestha <slokeshrestha131@gmail.com>
2020-01-15 12:57:24 -0600

    first commit

---
+++ lab00 - Eclipse test/src/test/lab00.java
@@ -0,0 +1,10 @@
+package test;
+
+public class lab00 {
+	public static void main(String[] args) {
+
+		for(int i = 0; i < 100; i++)
+			if(i > 90)
+				System.out.println("Hello, World" + ". Believe it!"); //intendation important
+	}
+}
